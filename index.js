50f8309063017e52b0d88684c8be5e5dd9e52a78
C J Silverio <ceejceej@gmail.com>
2016-10-23 15:09:13 -0700

    Let's name for the future. And generate a file per widget.

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
 
 var
+	dot     = require('dot'),
 	fs      = require('fs'),
 	Podcast = require('podcast'),
 	toml    = require('toml')
 	;
 
+dot.templateSettings.strip = false;
 var feedOptions = toml.parse(fs.readFileSync('./src/metadata.toml', 'utf8'));
 var feed = new Podcast(feedOptions);
-
-// read shows in directory
+var templatefn = dot.template(fs.readFileSync('./src/template.html'));
 
 var shows = fs.readdirSync('./src/sets/');
 shows.forEach(function handleShow(s)
@@ -18,6 +19,8 @@ shows.forEach(function handleShow(s)
 
 	var obj = toml.parse(fs.readFileSync(`src/sets/${s}`, 'utf8'));
 	feed.item(obj);
+	var txt = templatefn(obj);
+	fs.writeFileSync(`output/${s.replace(/\.toml$/, '')}.html`, txt, 'utf8');
 });
 
-fs.writeFileSync('./feed.rss', feed.xml('\t'), 'utf8');
+fs.writeFileSync('./output/feed.rss', feed.xml('\t'), 'utf8');
