5cb43e6d7b00c3eaadc75f0e82e8db754b28236f
C J Silverio <ceejceej@gmail.com>
2016-10-23 16:07:43 -0700

    More sets; better set data; better templates.

--- index.js
+++ index.js
@@ -10,17 +10,28 @@ var
 dot.templateSettings.strip = false;
 var feedOptions = toml.parse(fs.readFileSync('./src/metadata.toml', 'utf8'));
 var feed = new Podcast(feedOptions);
-var templatefn = dot.template(fs.readFileSync('./src/template.html'));
+var templatefn = dot.template(fs.readFileSync('./src/item.tmpl.html'));
+
+feedOptions.sets = [];
 
 var shows = fs.readdirSync('./src/sets/');
 shows.forEach(function handleShow(s)
 {
 	if (!s.endsWith('.toml')) return;
+	var setslug = s.replace(/\.toml$/, '');
+	console.log(setslug);
 
 	var obj = toml.parse(fs.readFileSync(`src/sets/${s}`, 'utf8'));
 	feed.item(obj);
 	var txt = templatefn(obj);
-	fs.writeFileSync(`output/${s.replace(/\.toml$/, '')}.html`, txt, 'utf8');
+	fs.writeFileSync(`output/${setslug}.html`, txt, 'utf8');
+	feedOptions.sets.push({slug: setslug, title: obj.title });
 });
 
 fs.writeFileSync('./output/feed.rss', feed.xml('\t'), 'utf8');
+
+var indexfn = dot.template(fs.readFileSync('./src/index.tmpl.html'));
+var txt = indexfn(feedOptions);
+fs.writeFileSync('output/index.html', txt, 'utf8');
+
+console.log('Done. Now rsync.');
